7d3f587d4d4d5759e7bb7d95376e5ce39ee43c7e
BobbyTherio <bobby2theriault@gmail.com>
2022-03-31 08:50:42 -0600

    addtask-form

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ITask } from 'src/app/interfaces/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getTasks() {
+    return this.httpClient.get<ITask[]>('http://localhost:3000/task');
+  }
+}
